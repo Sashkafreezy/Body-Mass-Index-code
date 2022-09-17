eeac4855e586a5f4ae22710889fa321acdd0ac88
«Sashkafeezy» <freezy41@yandex.ru>
2022-09-17 21:29:07 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         double weight = 98; // кг
         double height = 1.80; // м^2
         double calculate = service.calculate(weight, height);
-        double bmi = Math.round(service.calculate(weight , height)*100.0)/100.0;
+        double bmi = Math.round(service.calculate(weight, height) * 100.0) / 100.0;
         System.out.println("Ваш ИМТ равен " + bmi);
     }
 }
