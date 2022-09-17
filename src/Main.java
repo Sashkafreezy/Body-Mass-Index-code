284508e0ea10478eb5d11aa77ca1a09726ded9a3
«Sashkafeezy» <freezy41@yandex.ru>
2022-09-17 21:26:28 +0300

    Initial BMI code

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double weight = 98; // кг
+        double height = 1.80; // м^2
+        double calculate = service.calculate(weight, height);
+        double bmi = Math.round(service.calculate(weight , height)*100.0)/100.0;
+        System.out.println("Ваш ИМТ равен " + bmi);
+    }
+}
